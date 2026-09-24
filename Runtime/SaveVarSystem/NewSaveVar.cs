70563f19e84c25f4538b660e75934e9f74eb5fb0
Janek <jasiogamer@gmail.com>
2026-09-25 00:01:26 +0200

    dirtable savedata WIP

--- Runtime/SaveVarSystem/NewSaveVar.cs
+++ Runtime/SaveVarSystem/NewSaveVar.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using fefek5.Toys.Runtime;
 using UnityEngine;
 
 namespace fefek5.SaveDataVariable.Runtime
 {
+    public class DirtableSaveData
+    {
+        public SaveData Origin;
+        public SaveData Target;
+
+        
+    }
+    
     [Serializable]
-    public class NewSaveVar { }
+    public class NewSaveVar
+    {
+        internal static Dictionary<string, SaveData> SaveDatas = new();
+        internal static Dictionary<string, HashSet<SaveKey>> Dirties = new();
+
+        #region Properties
+
+        public bool IsDirty => Dirties.TryGetValue(Path,  out var keys) && keys.Contains(SaveKey);
+
+        public string Path => GetPath(RelativePath);
+
+        #endregion
+        
+        #region Inspector Fields
+        
+        [field: SerializeField] public SaveKey SaveKey { get; private set; }
+        
+        [field: SerializeField] public string RelativePath { get; private set; }
+
+        #endregion
+
+        internal void SetDirty()
+        {
+            if (IsDirty)
+                throw new InvalidOperationException("Value is arleady dirty!");
+
+            if (Dirties.TryGetValue(Path, out var keys)) keys.Add(SaveKey);
+            else Dirties[Path] = new HashSet<SaveKey> { SaveKey };
+        }
+        
+        internal virtual void RemoveDirty()
+        {
+            if (!IsDirty)
+                throw new InvalidOperationException("Value is not dirty!");
+            
+            Dirties[Path].Remove(SaveKey);
+        }
+
+        public void ApplyDirty()
+        {
+            if (!IsDirty)
+                throw new InvalidOperationException("Value is not dirty!");
+            
+            
+        }
+        
+        private static string GetPath(string relativePath) =>
+            System.IO.Path.GetFullPath(System.IO.Path.Combine(Application.persistentDataPath, relativePath));
+
+        public static bool IsSaveDataDirty(string path) => Dirties.ContainsKey(path);
+
+        public static void RemoveSaveDataDirty(string path)
+        {
+            if (!IsSaveDataDirty(path))
+                throw new InvalidOperationException("SaveData is not dirty!");
+            
+            Dirties.Remove(path);
+        }
+    }
 
     [Serializable]
     public class NewSaveVar<T> : NewSaveVar
@@ -17,34 +84,22 @@ namespace fefek5.SaveDataVariable.Runtime
             set => SetValue(value);
         }
 
-        public bool IsDirty => _value.hasValue;
-        
-        public string Path => GetPath(RelativePath);
-
         #region Inspector Fields
-        
-        [field: SerializeField] public SaveKey SaveKey { get; private set; }
         
         [field: SerializeField] public T DefaultValue { get; private set; }
         
-        [field: SerializeField] public string RelativePath { get; private set; }
-
         #endregion
 
-        #region Privete Fields
-
-        private HasValue<T> _value = new(default, false);
-
-        #endregion
+        private T _dirty;
         
         #region Getrers and Setters
 
-        public T GetValue() => _value.hasValue ? _value.value : GetSaveData().GetKey(SaveKey, DefaultValue);
+        public T GetValue() => TryGetDirtyValue(out var dirty) ? dirty : GetSaveData().GetKey(SaveKey, DefaultValue);
 
         public async Awaitable<T> GetValueAsync()
         {
-            if (_value.hasValue)
-                return _value.value;
+            if (TryGetDirtyValue(out var dirty))
+                return dirty;
             
             var saveData = await GetSaveDataAsync();
             
@@ -53,7 +108,8 @@ namespace fefek5.SaveDataVariable.Runtime
 
         public void SetValue(T value)
         {
-            _value.Set(default, false);
+            if (IsDirty)
+                RemoveDirty();
 
             var saveData = GetSaveData();
 
@@ -64,7 +120,8 @@ namespace fefek5.SaveDataVariable.Runtime
 
         public async Awaitable SetValueAsync(T value)
         {
-            _value.Set(default, false);
+            if (IsDirty)
+                RemoveDirty();
 
             var saveData = await GetSaveDataAsync();
 
@@ -73,26 +130,67 @@ namespace fefek5.SaveDataVariable.Runtime
             await saveData.SaveAsync(Path);
         }
 
-        public void SetValueWithoutNotifying(T value) => _value.Set(value, true);
+        public void SetValueWithoutNotifying(T value)
+        {
+            GetSaveData().SetKey(SaveKey, value);
+            SetDirty();
+        }
 
         public SaveData GetSaveData()
         {
-            var saveData = new SaveData();
+            if (SaveDatas.TryGetValue(Path, out var existingSaveData))
+                return existingSaveData;
+            
+            var newSaveData = new SaveData();
             
             if(File.Exists(Path))
-                saveData.Load(Path);
+                newSaveData.Load(Path);
+            
+            SaveDatas.Add(Path, newSaveData);
 
-            return saveData;
+            return newSaveData;
         }
 
         public async Awaitable<SaveData> GetSaveDataAsync()
         {
-            var saveData = new SaveData();
-
-            if (File.Exists(Path))
-                await saveData.LoadAsync(Path);
+            if (SaveDatas.TryGetValue(Path, out var existingSaveData))
+                return existingSaveData;
             
-            return saveData;
+            var newSaveData = new SaveData();
+            
+            if(File.Exists(Path))
+                await newSaveData.LoadAsync(Path);
+            
+            SaveDatas.Add(Path, newSaveData);
+
+            return newSaveData;
+        }
+
+        private bool TryGetDirtyValue(out T dirty)
+        {
+            if (!IsDirty)
+            {
+                dirty =  default;
+                return false;
+            }
+
+            dirty = GetDirtyValue();
+            return true;
+        }
+        
+        private T GetDirtyValue()
+        {
+            if (!IsDirty)
+                throw new InvalidOperationException("Value is not dirty!");
+
+            return _dirty;
+        }
+
+        internal override void RemoveDirty()
+        {
+            base.RemoveDirty();
+            
+            _dirty = default;
         }
 
         #endregion
@@ -140,8 +238,5 @@ namespace fefek5.SaveDataVariable.Runtime
         }
 
         #endregion
-        
-        private static string GetPath(string relativePath) => 
-            System.IO.Path.GetFullPath(System.IO.Path.Combine(Application.persistentDataPath, relativePath));
     }
 }
